bba9ee562dbc68029fb4d26deef9cc36df1b9e7f
Cedic <ced742@gmail.com>
2015-11-21 22:17:30 +0100

    test

---
+++ TP-DevOO/src/modele/DemandeDeLivraisonTest.java
@@ -0,0 +1,59 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemandeDeLivraisonTest {
+
+	@Test
+	public void testDemandeDeLivraison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKeyByValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifierTournee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimeLivraison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAjouteLivraison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMappingLivraisons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenererTableauArcs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculerTournee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculPlusCourtChemin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenereFeuilleDeRoute() {
+		fail("Not yet implemented");
+	}
+
+}

a5fe23fc7350c3e45160ff11ca02f2d775aeb637
Your Name <you@example.com>
2015-11-21 19:11:51 +0100

    oho

---
+++ TP-DevOO/src/modele/Observable2.java
@@ -0,0 +1,82 @@
+package modele;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Observable2 {
+
+    /**
+     * Default constructor
+     */
+    public Observable2() {
+    }
+
+
+    /**
+     * @param observer
+     */
+    protected void addObserver(Observer observer) {
+        // TODO implement here
+    }
+
+    /**
+     * @param obj
+     */
+    protected void notifyObservers(Object obj) {
+        // TODO implement here
+    }
+
+    /**
+     * @param observer
+     */
+    protected void deleteObserver(Observer observer) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    protected void deleteObservers() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    protected void notifyObservers() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    protected void setChanged() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    protected void clearChanged() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    protected Boolean hasChanged() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    protected Integer countObservers() {
+        // TODO implement here
+        return null;
+    }
+
+}

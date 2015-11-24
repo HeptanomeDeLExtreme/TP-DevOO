ac2bc9f20b597c468c6e83b0cb268955801a7f85
marion chalumeau <marion@DESKTOP-CH4NV5U>
2015-11-24 08:12:32 +0100

    Ajout methode compare

--- TP-DevOO/src/modele/Horaire.java
+++ TP-DevOO/src/modele/Horaire.java
@@ -36,6 +36,41 @@ public class Horaire {
      */
 	protected int seconde;
 	
+	/**
+	 * Compare 2 horaires
+	 * Utilisation : horaire1.compare(horaire2)
+	 * retourne 1 si horaire1>horaire2, 0 si horaire1=horaire2, -1 si horaire1<horaire2
+	 * @param Horaire horaire : horaire à comparer avec this
+	 * @return int
+	 */	
+	public int compare(Horaire horaire){
+		if(this.heure > horaire.getHeure()){
+			return 1;
+		}
+		else if (this.heure < horaire.getHeure()){
+			return -1;
+		}
+		else{
+			if (this.minute > horaire.getMinute()){
+				return 1;
+			}
+			else if (this.minute < horaire.getMinute()){
+				return -1;
+			}
+			else{
+				if (this.seconde > horaire.getSeconde()){
+					return 1;
+				}
+				else if (this.seconde < horaire.getSeconde()){
+					return -1;
+				}
+				else{
+					return 0;
+				}
+			}
+		}
+	}
+	
 	@Override
 	public String toString(){
 		String s = " h " + this.heure + " m " + this.minute+ " s " + this.seconde;

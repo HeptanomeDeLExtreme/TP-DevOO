1bb7a1b16f495fa8c38de353c409050f2fd61ea1
ealhad <34lh4d@gmail.com>
2015-11-24 09:50:36 +0100

    Dijkstra doc

--- TP-DevOO/src/modele/Dijkstra.java
+++ TP-DevOO/src/modele/Dijkstra.java
@@ -3,12 +3,17 @@ package modele;
 
 public class Dijkstra {
 
-	// Dijkstra's algorithm to find shortest path from s to all other nodes
+	/**
+	 * Algorithme de Dijkstra
+	 * @param G graphe pondéré traité
+	 * @param racine Noeud à partir duquel les coûts sont calculés
+	 * @return int[][] {tableau des distances, tableau des prédecesseurs}
+	 */
 	public static int[][] dijkstra(GraphePondere G, int racine) {
 		final int[] dist = new int[G.getNbNoeuds()]; // tableau des distances
 		final int[] pred = new int[G.getNbNoeuds()]; // tableau des
 														// prédécesseurs
-		final boolean[] visited = new boolean[G.getNbNoeuds()]; // couleurs
+		final boolean[] visite = new boolean[G.getNbNoeuds()]; // couleurs
 
 		for (int i = 0; i < dist.length; i++) {
 			dist[i] = Integer.MAX_VALUE;
@@ -17,19 +22,19 @@ public class Dijkstra {
 		dist[racine] = 0;
 		for (int i = 0; i < dist.length; i++) {
 
-			final int next = minVertex(dist, visited);
-			visited[next] = true;
+			final int suivant = calculSuivant(dist, visite);
+			visite[suivant] = true;
 
 			// The shortest path to next is dist[next] and via pred[next].
 
-			final int[] n = G.neighbors(next);
+			final int[] n = G.neighbors(suivant);
 
 			for (int j = 0; j < n.length; j++) {
 				final int v = n[j];
-				final int d = dist[next] + G.getWeight(next, v);
+				final int d = dist[suivant] + G.getWeight(suivant, v);
 				if (dist[v] > d) {
 					dist[v] = d;
-					pred[v] = next;
+					pred[v] = suivant;
 				}
 			}
 		}
@@ -38,11 +43,11 @@ public class Dijkstra {
 
 	}
 
-	private static int minVertex(int[] dist, boolean[] v) {
+	private static int calculSuivant(int[] dist, boolean[] v) {
 
 		int x = Integer.MAX_VALUE;
 
-		int y = -1; // graph not connected, or no unvisited vertices
+		int y = -1;
 
 		for (int i = 0; i < dist.length; i++) {
 			if (!v[i] && dist[i] < x) {
